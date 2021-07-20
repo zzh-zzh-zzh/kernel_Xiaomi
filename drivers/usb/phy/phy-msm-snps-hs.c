43316f0dc2d16d9fb28b1c7ecc7af02712fa67d6
lateautumn233 <3487467850@qq.com>
2021-07-20 10:28:24 +0800

    Reapply: Kernel: Xiaomi kernel changes for Redmi K30S Ultra, Mi 10 Ultra, Mi 10 Pro, Redmi K30 Pro and Mi 10 Android R
    The Patch based on QualComm release TAG:LA.UM.9.12.r1-08000-SMxx50.0
    The kernel config file used is apollo_user_defconfig, cas_user_defconfig, cmi_user_defconfig, lmi_user_defconfig and umi_user_defconfig
    
    Change-Id: Ia08733ebd9e4baf2f2297288d13abf55c3471b1c
    Signed-off-by: bsp-open <bsp-open@xiaomi.com>
    
    Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

--- drivers/usb/phy/phy-msm-snps-hs.c
+++ drivers/usb/phy/phy-msm-snps-hs.c
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
+<<<<<<< HEAD
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
+=======
+ * Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2021 XiaoMi, Inc.
+>>>>>>> 7457b7d207db (Reapply: Kernel: Xiaomi kernel changes for Redmi K30S Ultra, Mi 10 Ultra, Mi 10 Pro, Redmi K30 Pro and Mi 10 Android R)
  */
 
 #include <linux/module.h>
@@ -20,6 +25,8 @@
 #include <linux/usb/phy.h>
 #include <linux/reset.h>
 #include <linux/debugfs.h>
+/* add for get hw country */
+#include <soc/qcom/socinfo.h>
 
 #define USB2_PHY_USB_PHY_UTMI_CTRL0		(0x3c)
 #define OPMODE_MASK				(0x3 << 3)
@@ -106,6 +113,9 @@ struct msm_hsphy {
 	int			*param_override_seq;
 	int			param_override_seq_cnt;
 
+	int			*global_param_override_seq;
+	int			global_param_override_seq_cnt;
+
 	void __iomem		*phy_rcal_reg;
 	u32			rcal_mask;
 
@@ -121,6 +131,8 @@ struct msm_hsphy {
 	u8			param_ovrd1;
 	u8			param_ovrd2;
 	u8			param_ovrd3;
+
+	uint32_t hw_country;
 };
 
 static void msm_hsphy_enable_clocks(struct msm_hsphy *phy, bool on)
@@ -388,6 +400,12 @@ static int msm_hsphy_init(struct usb_phy *uphy)
 	if (phy->param_override_seq)
 		hsusb_phy_write_seq(phy->base, phy->param_override_seq,
 				phy->param_override_seq_cnt, 0);
+
+	/* set parameter ovrride  if needed */
+	if (phy->hw_country == (uint32_t)CountryGlobal
+			&& phy->global_param_override_seq)
+		hsusb_phy_write_seq(phy->base, phy->global_param_override_seq,
+				phy->global_param_override_seq_cnt, 0);
 
 	if (phy->pre_emphasis) {
 		u8 val = TXPREEMPAMPTUNE0(phy->pre_emphasis) &
@@ -831,6 +849,34 @@ static int msm_hsphy_probe(struct platform_device *pdev)
 		}
 	}
 
+	phy->global_param_override_seq_cnt = of_property_count_elems_of_size(
+					dev->of_node,
+					"qcom,global-param-override-seq",
+					sizeof(*phy->global_param_override_seq));
+	if (phy->global_param_override_seq_cnt > 0) {
+		phy->global_param_override_seq = devm_kcalloc(dev,
+					phy->global_param_override_seq_cnt,
+					sizeof(*phy->global_param_override_seq),
+					GFP_KERNEL);
+		if (!phy->global_param_override_seq)
+			return -ENOMEM;
+
+		if (phy->global_param_override_seq_cnt % 2) {
+			dev_err(dev, "invalid param_override_seq_len\n");
+			return -EINVAL;
+		}
+
+		ret = of_property_read_u32_array(dev->of_node,
+				"qcom,global-param-override-seq",
+				phy->global_param_override_seq,
+				phy->global_param_override_seq_cnt);
+		if (ret) {
+			dev_err(dev, "qcom,global-param-override-seq read failed %d\n",
+				ret);
+			return ret;
+		}
+	}
+
 	ret = of_property_read_u32_array(dev->of_node, "qcom,vdd-voltage-level",
 					 (u32 *) phy->vdd_levels,
 					 ARRAY_SIZE(phy->vdd_levels));
@@ -860,6 +906,9 @@ static int msm_hsphy_probe(struct platform_device *pdev)
 		ret = PTR_ERR(phy->vdda18);
 		goto err_ret;
 	}
+
+	phy->hw_country = get_hw_country_version();
+	dev_err(dev, "phy hw_country: %d\n", phy->hw_country);
 
 	mutex_init(&phy->phy_lock);
 	platform_set_drvdata(pdev, phy);
@@ -903,6 +952,9 @@ static int msm_hsphy_remove(struct platform_device *pdev)
 
 	msm_hsphy_enable_clocks(phy, false);
 	msm_hsphy_enable_power(phy, false);
+
+	kfree(phy);
+
 	return 0;
 }
 
